dc0f81913dc10c124797a9d6dff9a71c632b4323
Lars Brinkhoff <lars@nocrew.org>
2017-08-12 22:16:07 +0200

    Program an AVR device with avrdude.

--- target/avr/target.mk
+++ target/avr/target.mk
@@ -2,3 +2,6 @@ test-image: image
 	simulavr -D -d at90s2313 $< > $@ 2>&1
 	! grep "Unknown opcode" $@
 	grep "BREAK POINT" $@
+
+upload: image
+	sudo avrdude -C target/avr/avrdude.conf -c usbtiny -p attiny85 -U flash:w:image:r -P usb
