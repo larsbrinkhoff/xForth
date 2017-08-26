b4fc6e7d99a6b42cf47bdf8523eaaf5c03f8359f
Lars Brinkhoff <lars@nocrew.org>
2017-08-26 09:50:39 +0200

    Test STM8 in Travis.

--- target/stm8/target.mk
+++ target/stm8/target.mk
@@ -1,2 +1,6 @@
-test-image: image
-	true
+START=0x8000
+
+test-image: image.hex
+	sstm8 -J -tS103 -C target/stm8/test.ucsim > $@
+	! grep "Invalid instruction 0x0005" $@
+	grep "Invalid instruction 0x008b" $@
