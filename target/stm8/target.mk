1de946489c1d001b6fa58770d18b9fe902f32c40
Lars Brinkhoff <lars@nocrew.org>
2017-09-29 14:16:44 +0200

    Upload an image to an STM8.

--- target/stm8/target.mk
+++ target/stm8/target.mk
@@ -4,3 +4,7 @@ test-image: image.hex
 	sstm8 -J -tS103 -C $(TDIR)/test.ucsim > $@
 	! grep "Invalid instruction 0x0005" $@
 	grep "Invalid instruction 0x008b" $@
+
+upload: image.hex
+	sudo stm8flash -c stlinkv2 -p stm8s103f3 -w $<
+
