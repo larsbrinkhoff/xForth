66affd7b680c1d3d88c22d697a4c2b1ecb21a985
Lars Brinkhoff <lars@nocrew.org>
2017-09-30 20:34:02 +0200

    Upload flash image to a Launchpad board.

--- target/msp430/target.mk
+++ target/msp430/target.mk
@@ -3,3 +3,6 @@ OPTS = -msp430 -break_io 0x0000 -set_pc $(START) -bin -address $(START)
 
 test-image: image
 	naken_util $(OPTS) -run image > $@
+
+upload: image.hex
+	sudo mspdebug rf2500 "prog $<"
