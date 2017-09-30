8d804ff106885b9441e191fa8c8ce84fd50d63a4
Lars Brinkhoff <lars@nocrew.org>
2017-09-30 15:08:56 +0200

    Upload flash image to STM32 device.

--- target/thumb/target.mk
+++ target/thumb/target.mk
@@ -1,4 +1,10 @@
+# This is for STM32L0x1 devices.
+START=0x08000000
+
 test-image: image
 	thumbulator -m 0 -d $< > $@ 2>&1
 	! grep "bkpt 0xFF" $@
 	grep "bkpt 0x00" $@
+
+upload: image
+	sudo st-flash write $< $(START)
