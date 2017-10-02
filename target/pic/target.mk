7e6f4c7ade2316961d8f27bcd800848ba76a192e
Lars Brinkhoff <lars@nocrew.org>
2017-10-02 12:45:11 +0200

    Upload an image to a Microchip device.

--- target/pic/target.mk
+++ target/pic/target.mk
@@ -4,3 +4,6 @@ test-image: image.hex
 	gpsim -c $(TDIR)/test.stc > $@
 	! grep INVREG_63 $@
 	grep INVREG_60 $@
+
+upload: image.hex
+	sudo /opt/microchip/mplabx/v4.01/mplab_ide/bin/mdb.sh $(TDIR)/upload.mdb
