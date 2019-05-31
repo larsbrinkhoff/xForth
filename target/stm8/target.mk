0f735b65b3664eac943defa782f18eebc79a3602
Lars Brinkhoff <lars@nocrew.org>
2019-05-31 10:39:49 +0200

    Update messages from ucsim STM-8 simulator.

--- target/stm8/target.mk
+++ target/stm8/target.mk
@@ -2,8 +2,8 @@ START=0x8000
 
 test-image: image.hex
 	sstm8 -J -tS103 -C $(TDIR)/test.ucsim > $@
-	! grep "Invalid instruction 0x0005" $@
-	grep "Invalid instruction 0x008b" $@
+	! grep "05             UNKNOWN/INVALID" $@
+	grep "8b             break" $@
 
 upload: image.hex
 	sudo stm8flash -c stlinkv2 -p stm8s103f3 -w $<
